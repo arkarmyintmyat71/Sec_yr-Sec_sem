69b3cb85f921db63716c8f6f249647fa57c1133e
Arkar Myint Myat <arkarmyintmyat71@gmail.com>
2026-02-01 15:25:14 +0700

    bug fix and responsive for auth page for cos

--- Cafe-Order-System/src/main/resources/static/js/auth.js
+++ Cafe-Order-System/src/main/resources/static/js/auth.js
@@ -1,38 +1,15 @@
-let isLogin = true;
-
 function toggleForm() {
-    const loginForm = document.getElementById("loginForm");
-    const registerForm = document.getElementById("registerForm");
+    const login = document.getElementById("loginForm");
+    const register = document.getElementById("registerForm");
+    const title = document.getElementById("formTitle");
 
-    const leftIcon = document.getElementById("leftIcon");
-    const leftTitle = document.getElementById("leftTitle");
-    const leftText = document.getElementById("leftText");
-    const leftBtnIcon = document.getElementById("leftBtnIcon");
-    const leftBtnText = document.getElementById("leftBtnText");
-
-    if (isLogin) {
-        // Show Register
-        loginForm.style.display = "none";
-        registerForm.style.display = "block";
-
-        leftIcon.className = "bi bi-person-plus-fill auth-logo";
-        leftTitle.innerText = "Staff Registration";
-        leftText.innerText = "Create an account for cafe employees.";
-        leftBtnIcon.className = "bi bi-box-arrow-in-right";
-        leftBtnText.innerText = " Login";
+    if (login.style.display === "none") {
+        login.style.display = "block";
+        register.style.display = "none";
     } else {
-        // Show Login
-        loginForm.style.display = "block";
-        registerForm.style.display = "none";
-
-        leftIcon.className = "bi bi-cup-hot-fill auth-logo";
-        leftTitle.innerText = "Cafe Order System";
-        leftText.innerText = "Staff login to manage orders and billing.";
-        leftBtnIcon.className = "bi bi-person-plus";
-        leftBtnText.innerText = " Register";
+        login.style.display = "none";
+        register.style.display = "block";
     }
-
-    isLogin = !isLogin;
 }
 
 function calculateAge(dob) {
@@ -40,9 +17,9 @@ function calculateAge(dob) {
     const today = new Date();
 
     let age = today.getFullYear() - birthDate.getFullYear();
-    const monthDiff = today.getMonth() - birthDate.getMonth();
+    const m = today.getMonth() - birthDate.getMonth();
 
-    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
         age--;
     }
 
